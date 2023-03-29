08e103e9f94f1ffd911444bcefb8ac4973620282
Sujin Lee <sujinbonnaud@gmail.com>
2023-03-28 22:06:27 -0500

    divide info box as for diff comp

---
+++ phys1/Assets/velobox.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class infobox : MonoBehaviour
+{
+    // public static double mess = 60;
+    public static double velo = 1;
+    // public static double accel = 0;
+    // public static double grav = 9.8;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // GetComponent<Text>().text = mess.ToString();
+        GetComponent<Text>().text = velo.ToString();
+        // GetComponent<Text>().text = accel.ToString();
+        // GetComponent<Text>().text = grav.ToString();
+    }
+}
