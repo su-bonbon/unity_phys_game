6e576db9a931870adacc9592c2fd43446eac118f
Sujin Lee <sujinbonnaud@gmail.com>
2023-03-29 19:34:53 -0500

    infobox function completed

--- phys1/Assets/bganima.cs
+++ phys1/Assets/bganima.cs
@@ -8,27 +8,34 @@ public class bganima : MonoBehaviour
     private Animator anim;
 
     private float distance = 0;
+    private float speed = 0;
+    private float timer = 0;
     public Text distanceUI;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
+
     }
 
     
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
         if (Input.GetKeyDown("space"))
         {
             anim.speed = 15f;
-            distance += 0.1f;
+            distance += 0.5f;
+            speed = distance / timer;
         }
         else
         {
             anim.speed = 0f;
-            distanceUI.text = "distance: " + distance.ToString("F2") + " m";
+            distanceUI.text = "Distance: " + distance.ToString("F2") + " m\n" +
+                              "Time: " + timer.ToString("F2") + " s\n" +
+                              "Speed: " + speed.ToString("F2") + "m/s";
 
         }
     }
